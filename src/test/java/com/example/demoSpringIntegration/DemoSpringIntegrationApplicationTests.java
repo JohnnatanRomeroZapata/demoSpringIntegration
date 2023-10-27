c5aa6d9014a12e81b23c95941392c13242cc4d72
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-26 21:06:57 -0400

    project init

---
+++ src/test/java/com/example/demoSpringIntegration/DemoSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoSpringIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
